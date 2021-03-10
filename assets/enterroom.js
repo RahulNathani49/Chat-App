9932f53411c65585b148f0dc6534c701b1941826
Rahul Nathani <nathanirahul49@gmail.com>
2021-03-10 17:30:05 +0530

    UI Updated

--- assets/enterroom.js
+++ assets/enterroom.js
@@ -2,11 +2,35 @@ var enterroom = document.getElementById("enterroom");
 var proceed = document.getElementById("proceed");
 enterroom.addEventListener('click',function(){
     var roomcode=document.getElementById("roomcode").value;
+    if(roomcode.includes("/")==true || roomcode==""){
+        alert("Wrong Path or Invalid Characters Entered");
+        document.getElementById("roomcode").value="";
+    }
+    else if(roomcode.includes(" ")==true){
+        alert("Wrong Path or Invalid Characters Entered");
+        document.getElementById("roomcode").value="";
+    }
+    else if(roomcode.includes("'")==true){
+        alert("Wrong Path or Invalid Characters Entered");
+        document.getElementById("roomcode").value="";
+    }
+    else if(roomcode.includes('"')==true){
+        alert("Wrong Path or Invalid Characters Entered");
+        document.getElementById("roomcode").value="";
+    }
+    else if(roomcode.includes('*')==true){
+        alert("Wrong Path or Invalid Characters Entered");
+        document.getElementById("roomcode").value="";
+    }
+    else{
+ 
     var redirecturl="/room/";
     window.open(redirecturl+roomcode,"_self");
     document.getElementById("roomcode").value="";
+           
+}
 })
 proceed.addEventListener('click',function(){
     var redirecturl="/room/";
     window.open(redirecturl,"_self");
-})
+})  
